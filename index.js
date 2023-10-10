a6a78cabb0562bc0abbd2f2dbb952389dce04aa4
boukhaddejatasnime <boukhaddejatas@gmail.com>
2023-10-10 10:34:57 +0200

    yet

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function Movies(name,images,description,cast,price){
         date:new Date(),
         description:description,
         cast:cast,
-        price:price()
+        price:price
     }
 }
 
@@ -13,8 +13,8 @@ function Movies(name,images,description,cast,price){
 var movie1=Movies("choose your place",["images/movie.avif","images/drive.webp","images/canape.jpg","images/cinema.jpg"],"Romance/Drama","Kate Winslet/Leonardo DiCaprio",10)
 console.log(movie1,"img")
 $('#change').append(`<h3>${movie1.date}</h3>
-<p>Price : ${movie1.price()}</p>
 `)
+
 
 function price(){
     if(movie1.images[1]){
@@ -48,4 +48,52 @@ var change=changeimg(movie1.images)
 $('#img1').on('click',function(){
   var getnextindex=change()
   this.src=getnextindex
-})
+}) /////
+
+
+/*function img{
+    function getimg (images){
+        this.images=images;
+        this.index=0
+        this.imageElement=document.getElementById("img1")
+        this.descriptionElement=document.getElementById("description")
+        this.priceElement=document.getElementById("price")
+        this.prevButton=document.getElementById('previous')
+        this.nextButton=document.getElementById("next")
+    
+
+this.showImage(this.index)
+this.prevButton.addEventListener("click",this.Sprevious.console.log(this))
+this.nextButton.addEventListener("click",this.Snext.console.log(this))
+    }
+showImage(i){
+    if(i>=0 && i<this.images.length){
+        var image=this.images[i]
+        this.imageElement.src=image.src
+        this.descriptionElement.textContent=image.description
+        this.priceElement.textContent="price: $${img1.price}"
+        
+        
+    }
+}
+Sprevious(){
+    if(this.index>0){
+        this.index--
+        this.showImage(this.index)
+    }
+}
+Snext(){
+    if(this.index< this.images.length-1){
+        this.index++
+        this.showImage(this.index)
+    }
+
+}
+
+}
+var images=[
+    {src:"images/cinema.jpg",description:"standard",price:"8dt"},
+    {src:"images/canape.jpg",description:"comfortable",price:"15dt"},
+    {src:"images/drve.webp",description:"from your car",price:"10dt"}
+]
+ var change=new img(imags)*/
